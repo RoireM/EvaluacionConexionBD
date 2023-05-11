73df55153f0157874d6f9a750572cc073950d256
RoireM <roirevh@gmail.com>
2023-05-11 14:13:06 -0600

    Implementacion de guardado

---
+++ app/src/main/java/ni/edu/uca/sistematicopersistencia/data/database/dao/UsuarioDao.kt
@@ -0,0 +1,4 @@
+package ni.edu.uca.sistematicopersistencia.data.database.dao
+
+class UsuarioDao {
+}
--- app/src/main/java/ni/edu/uca/sistematicopersistencia/guardarProducto.kt
+++ app/src/main/java/ni/edu/uca/sistematicopersistencia/guardarProducto.kt
@@ -5,22 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import ni.edu.uca.sistematicopersistencia.data.database.BaseDatos
+import ni.edu.uca.sistematicopersistencia.data.database.entities.EntityProducto
+import ni.edu.uca.sistematicopersistencia.databinding.FragmentGuardarProductoBinding
+import java.lang.Double.parseDouble
+import java.lang.Float.parseFloat
+import java.lang.Integer.parseInt
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [guardarProducto.newInstance] factory method to
- * create an instance of this fragment.
- */
 class guardarProducto : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    private lateinit var binding: FragmentGuardarProductoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +38,20 @@ class guardarProducto : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guardar_producto, container, false)
+        binding = FragmentGuardarProductoBinding.inflate(inflater, container, false)
+        binding.btnGuardar.setOnClickListener {
+            val nombre = binding.nombre.text.toString()
+            val precio = parseDouble(binding.precio.text.toString())
+            val existencias = parseInt(binding.existencia.text.toString())
+            val producto = EntityProducto(0, nombre, precio, existencias)
+
+            lifecycleScope.launch {
+                BaseDatos.obtBaseDatos(this@guardarProducto.requireContext()).productoDao().insertarReg(producto)
+            }
+            // BaseDatos(this@guardarProducto).productoDao().insertarReg(nombre)
+
+        }
+        return binding.root
     }
 
     companion object {
